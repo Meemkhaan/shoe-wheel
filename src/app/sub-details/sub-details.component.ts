09f4deafa484eb323ebbfcb9901cbbcca9cbd98d
Tayyab Siraj <muhammadtayab3022@gmail.com>
2022-05-11 19:16:57 +0500

    initial

--- src/app/sub-details/sub-details.component.ts
+++ src/app/sub-details/sub-details.component.ts
@@ -1,36 +1,42 @@
-import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import{FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 @Component({
   selector: 'app-sub-details',
   templateUrl: './sub-details.component.html',
   styleUrls: ['./sub-details.component.css'],
 })
-export class SubDetailsComponent implements OnInit, AfterViewInit {
+export class SubDetailsComponent implements OnInit {
+  tc_vinyl: any;
+  tc_sports: any;
+  tc_microcell: any;
+  tc_leather: any;
+  tc_evaperferated: any;
+  tc_xstatic: any;
   constructor(private FormBuilder: FormBuilder) {}
-  thirdLayerOptions!:FormGroup
-  @ViewChild('thirdLayerForm') thirdLayerForms!:ElementRef;
+  thirdLayerOptions!: FormGroup;
   ngOnInit() {
-    this.thirdLayerOptions = this.FormBuilder.group({
-      vinylopt: [''],
-      sportsopt: [''],
-      microcellopt: [''],
-      leatheropt: [''],
-      evaperferatedopt: [''],
-      xstaticopt: [''],
-      suedoopt:[''],
-      //! Underlay
-      mivinylopt: [''],
-      misuedeopt: [''],
-      //! Microcell
-      mimicrocellopt: [''], 
-    })
+    this.tc_vinyl = localStorage.getItem('tc_vinyl_options');
+    this.tc_sports = localStorage.getItem('tc_sports_options');
+    this.tc_microcell = localStorage.getItem('tc_microcell_options');
+    this.tc_leather = localStorage.getItem('tc_leather_options');
+    this.tc_evaperferated = localStorage.getItem('tc_evaperferated_options');
+    this.tc_xstatic = localStorage.getItem('tc_xstatic_options');
 
     $(function () {
       $('.item').hide();
-
       let activatedRoute = window.location.pathname;
-      //! ALL TOPCOVERS FIELDS CONDITION BASED ON ROUTE PARAM
       if (activatedRoute.match('tc-vinyl')) {
         $('#vinyl > .item').show();
       } else if (activatedRoute.match('tc-sports')) {
@@ -46,18 +52,16 @@ export class SubDetailsComponent implements OnInit, AfterViewInit {
       } else if (activatedRoute.match('tc-sudeo')) {
         $('#suedo > .item').show();
       }
-      //! ALL MIDDLE LAYER FIELDS CONDITION BASED ON ROUTE PARAM
+
       if (activatedRoute.match('mi--vinyl')) {
         $('#mi-vinyl > .item').show();
-      } else if (activatedRoute.match('mi--suede')){
+      } else if (activatedRoute.match('mi--suede')) {
         $('#mi-suede > .item').show();
-      } else if (activatedRoute.match('mi--microcell')){
+      } else if (activatedRoute.match('mi--microcell')) {
         $('#mi-microcell > .item').show();
       }
 
-      //! ALL SHEEL TYPES AND CUSION FIELDS BASED ON ROUTE PARAM
-
-      if(activatedRoute.match('sheel-types-cushion')){
+      if (activatedRoute.match('sheel-types-cushion')) {
         $('#sheel-types-cushion > .item').hide();
       }
 
@@ -88,9 +92,81 @@ export class SubDetailsComponent implements OnInit, AfterViewInit {
       });
     });
   }
-  ngAfterViewInit() {
-    console.log(this.thirdLayerForms.nativeElement);
-    
+
+  onItemChange(value: any) {
+    this.tc_vinyl = value;
+    localStorage.setItem('tc_vinyl_options', this.tc_vinyl);
   }
+
+  onItemChange1(value: any) {
+    this.tc_sports = value;
+    localStorage.setItem('tc_sports_options', this.tc_sports);
+  }
+
+  onItemChange2(value: any) {
+    this.tc_microcell = value;
+    localStorage.setItem('tc_microcell_options', this.tc_microcell);
+  }
+
+  onItemChange3(value: any) {
+    this.tc_leather = value;
+    localStorage.setItem('tc_leather_options', this.tc_leather);
+  }
+
+  onItemChange4(value: any) {
+    this.tc_evaperferated = value;
+    localStorage.setItem('tc_evaperferated_options', this.tc_evaperferated);
+  }
+
+  onItemChange5(value: any) {
+    this.tc_xstatic = value;
+    localStorage.setItem('tc_xstatic_options', this.tc_xstatic);
+  }
+
+  //
+  tc_vinyl_options = [
+    { id: 1, value: 'Black' },
+    { id: 2, value: 'Navy' },
+    { id: 3, value: 'Brown' },
+    { id: 4, value: 'Red' },
+    { id: 5, value: 'Grey' },
+  ];
+
+  tc_sports_options = [
+    { id: 1, value: 'Black' },
+    { id: 2, value: 'Navy' },
+    { id: 3, value: 'Green' },
+    { id: 4, value: 'Red' },
+    { id: 5, value: 'Bamboo' },
+  ];
+
+  tc_microcell_options = [
+    { id: 1, value: 'Black' },
+    { id: 2, value: 'Navy' },
+    { id: 3, value: 'Red' },
+    { id: 4, value: 'Green' },
+    { id: 5, value: 'Brown' },
+    { id: 6, value: 'Yellow' },
+  ];
+
+  tc_leather_options = [
+    { id: 1, value: 'Black' },
+    { id: 2, value: 'Navy' },
+    { id: 3, value: 'Green' },
+  ];
+
+  tc_evaperferated_options = [
+    { id: 1, value: 'Black' },
+    { id: 2, value: 'Navy' },
+  ];
+
+  
+  tc_suedo_options = [
+    { id: 1, value: 'Black' },
+    { id: 2, value: 'Tan' },
+    { id: 3, value: 'Brown' },
+  ];
+
+
 
 }

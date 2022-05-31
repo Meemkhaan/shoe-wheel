829352e5f28295f371616cf268bbc9abfaef05d4
Tayyab Siraj <muhammadtayab3022@gmail.com>
2022-05-31 23:54:52 +0500

    changes

--- src/app/fourth-layer-options/fourth-layer-options.component.ts
+++ src/app/fourth-layer-options/fourth-layer-options.component.ts
@@ -24,11 +24,31 @@ export class FourthLayerOptionsComponent implements OnInit {
   //
   deep_heel_cup: any;
   //
-  first_meet_cut_out_left:any; 
-  first_meet_cut_out_right:any; 
+  first_meet_cut_out_left: any;
+  first_meet_cut_out_right: any;
   //
-  first_ray_cut_out_left:any; 
-  first_ray_cut_out_right:any; 
+  first_ray_cut_out_left: any;
+  first_ray_cut_out_right: any;
+  //
+  heel_center_pocket_left: any;
+  heel_center_pocket_right: any;
+  //
+  fascia_grove_left: any;
+  fascia_grove_right: any;
+  //
+  out_toeing: any;
+  in_toeing: any;
+  //
+  kirbey_skive_medical_position_left: any;
+  kirbey_skive_medical_position_left_no: any;
+  kirbey_skive_medical_position_right: any;
+  kirbey_skive_medical_position_right_no: any;
+
+  //
+  kirbey_skive_lateral_position_left: any;
+  kirbey_skive_lateral_position_left_no: any;
+  kirbey_skive_lateral_position_right: any;
+  kirbey_skive_lateral_position_right_no: any;
 
   constructor() {}
   ngOnInit() {
@@ -43,9 +63,22 @@ export class FourthLayerOptionsComponent implements OnInit {
         $('#cusion > .item').show();
       } else if (activatedRoute.match('types-of-sheel')) {
         $('#sheel-types > .item').show();
-      } else if(activatedRoute.match('first-meet-cut-out')){
+      } else if (activatedRoute.match('first-meet-cut-out')) {
         $('#first-meet-cut-out > .item').show();
-        
+      } else if (activatedRoute.match('first-ray-cut-out')) {
+        $('#first-ray-cut-out > .item').show();
+      } else if (activatedRoute.match('heel-center-pocket')) {
+        $('#heel-center-pocket > .item').show();
+      } else if (activatedRoute.match('heel-center-pocket')) {
+        $('#heel-center-pocket > .item').show();
+      } else if (activatedRoute.match('fascia-grove')) {
+        $('#fascia-grove > .item').show();
+      } else if (activatedRoute.match('induced-toeing')) {
+        $('#induced-toeing > .item').show();
+      } else if (activatedRoute.match('kirby-skive-lateral')) {
+        $('#kirby-skive-lateral > .item').show();
+      } else if (activatedRoute.match('kirby-skive-medial')) {
+        $('#kirby-skive-medial > .item').show();
       }
 
       let radius = 270;
@@ -105,8 +138,59 @@ export class FourthLayerOptionsComponent implements OnInit {
 
     //
     this.deep_heel_cup = localStorage.getItem('deep_heel_cup');
-    this.first_meet_cut_out_left = localStorage.getItem('first_meet_cut_out_left');
-    this.first_meet_cut_out_right = localStorage.getItem('first_meet_cut_out_right');
+    //
+    this.first_meet_cut_out_left = localStorage.getItem(
+      'first_meet_cut_out_left'
+    );
+    this.first_meet_cut_out_right = localStorage.getItem(
+      'first_meet_cut_out_right'
+    );
+    //
+    this.first_ray_cut_out_left = localStorage.getItem(
+      'first_ray_cut_out_left'
+    );
+    this.first_ray_cut_out_right = localStorage.getItem(
+      'first_ray_cut_out_right'
+    );
+    //
+    this.heel_center_pocket_left = localStorage.getItem(
+      'heel_center_pocket_left'
+    );
+    this.heel_center_pocket_right = localStorage.getItem(
+      'heel_center_pocket_right'
+    );
+    //
+    this.fascia_grove_left = localStorage.getItem('fascia_grove_left');
+    this.fascia_grove_right = localStorage.getItem('fascia_grove_right');
+    //
+    this.out_toeing = localStorage.getItem('induced_out_toening');
+    this.in_toeing = localStorage.getItem('induced_in_toening');
+    //
+    this.kirbey_skive_lateral_position_left = localStorage.getItem(
+      'kirbey_skive_lateral_position_left'
+    );
+    this.kirbey_skive_lateral_position_left_no = localStorage.getItem(
+      'kirbey_skive_lateral_position_left_no'
+    );
+    this.kirbey_skive_lateral_position_right = localStorage.getItem(
+      'kirbey_skive_lateral_position_right'
+    );
+    this.kirbey_skive_lateral_position_right_no = localStorage.getItem(
+      'kirbey_skive_lateral_position_right_no'
+    );
+    //
+    this.kirbey_skive_medical_position_left = localStorage.getItem(
+      'kirbey_skive_medical_position_left'
+    );
+    this.kirbey_skive_medical_position_left_no = localStorage.getItem(
+      'kirbey_skive_medical_position_left_no'
+    );
+    this.kirbey_skive_medical_position_right = localStorage.getItem(
+      'kirbey_skive_medical_position_right'
+    );
+    this.kirbey_skive_medical_position_right_no = localStorage.getItem(
+      'kirbey_skive_medical_position_right_no'
+    );
   }
 
   //
@@ -160,8 +244,7 @@ export class FourthLayerOptionsComponent implements OnInit {
     if (values == 'none' && value.target.checked == true) {
       console.log(values);
       this.tcsc_cusion['none'] = 'none)';
-    } 
-    else if (values == 'none' && value.target.checked == false) {
+    } else if (values == 'none' && value.target.checked == false) {
       delete this.tcsc_cusion['none'];
     }
 
@@ -186,9 +269,14 @@ export class FourthLayerOptionsComponent implements OnInit {
     localStorage.setItem('tcsc_cusion', JSON.stringify(this.tcsc_cusion));
   }
 
-
   test2(value: any, options: any) {
     value == true
+      ? localStorage.setItem(`${options}`, value)
+      : localStorage.removeItem(`${options}`);
+  }
+
+  test(value: any, options: any) {
+    value > 0
       ? localStorage.setItem(`${options}`, value)
       : localStorage.removeItem(`${options}`);
   }
@@ -196,23 +284,81 @@ export class FourthLayerOptionsComponent implements OnInit {
   deep_heel_cup_change(value: any) {
     localStorage.setItem('deep_heel_cup', value);
   }
-// 
-  first_meet_cut_out_left_change(value: any){
+  //
+  first_meet_cut_out_left_change(value: any) {
     this.test2(value.target.checked, 'first_meet_cut_out_left');
   }
-  
-  first_meet_cut_out_right_change(value: any){
+
+  first_meet_cut_out_right_change(value: any) {
     this.test2(value.target.checked, 'first_meet_cut_out_right');
   }
-  // 
-  first_ray_cut_out_left_change(value: any){
+  //
+  first_ray_cut_out_left_change(value: any) {
     this.test2(value.target.checked, 'first_ray_cut_out_left');
   }
-  
-  first_ray_cut_out_right_change(value: any){
+
+  first_ray_cut_out_right_change(value: any) {
     this.test2(value.target.checked, 'first_ray_cut_out_right');
   }
+  //
+  heel_center_pocket_left_change(value: any) {
+    this.test2(value.target.checked, 'heel_center_pocket_left');
+  }
 
+  heel_center_pocket_right_change(value: any) {
+    this.test2(value.target.checked, 'heel_center_pocket_right');
+  }
+  //
+  fascia_grove_left_change(value: any) {
+    this.test2(value.target.checked, 'fascia_grove_left');
+  }
+
+  fascia_grove_right_change(value: any) {
+    this.test2(value.target.checked, 'fascia_grove_right');
+  }
+
+  //
+  out_toeing_change(value: any) {
+    this.test2(value.target.checked, 'induced_out_toening');
+  }
+
+  in_toeing_change(value: any) {
+    this.test2(value.target.checked, 'induced_in_toening');
+  }
+  //
+  kirbey_skive_lateral_position_right_change(value: any) {
+    this.test2(value.target.checked, 'kirbey_skive_lateral_position_right');
+    console.log(value.target.checked);
+    
+  }
+  kirbey_skive_lateral_position_left_change(value: any) {
+    this.test2(value.target.checked, 'kirbey_skive_lateral_position_left');
+    console.log(value.target.checked);
+  }
+  kirbey_skive_lateral_position_right_no_change(value: any) {
+    this.test(value, 'kirbey_skive_lateral_position_right');
+    
+  }
+  kirbey_skive_lateral_position_left_no_change(value: any) {
+    this.test(value, 'kirbey_skive_lateral_position_left');
+  }
+  //
+  kirbey_skive_medical_position_right_change(value: any) {
+    this.test2(value.target.checked, 'kirbey_skive_medical_position_right');
+    console.log(value.target.checked);
+    
+  }
+  kirbey_skive_medical_position_left_change(value: any) {
+    this.test2(value.target.checked, 'kirbey_skive_medical_position_left');
+    console.log(value.target.checked);
+  }
+  kirbey_skive_medical_position_right_no_change(value: any) {
+    this.test(value, 'kirbey_skive_medical_position_right');
+    
+  }
+  kirbey_skive_medical_position_left_no_change(value: any) {
+    this.test(value, 'kirbey_skive_medical_position_left');
+  }
 
   //
   deep_heel_cup_options = [
